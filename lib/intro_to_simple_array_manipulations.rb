29072be7c20222ffcc7e2b326455fddf2d33ed09
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-21 05:54:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array,string)
+  updated_array = array.push(string)
+end
+
+def using_unshift(array,string)
+  updated_array = array.unshift(string)
+end
+
+def using_pop(array)
+  updated_array = array.pop
+end
+
+def pop_with_args(array)
+  updated_array = array.pop(2)
+end
+
+def using_shift(array)
+  udpated_array = array.shift
+end
+
+def shift_with_args(array)
+  updated_array = array.shift(2)
+end
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+def using_insert(array,element)
+  array.insert[4] = element
+end
